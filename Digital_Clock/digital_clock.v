0781fec08de9d537aeb1a0fd0858b6da0336dbc8
Sumanta Kumar Dutta <84955195+SumantaDutta5@users.noreply.github.com>
2025-12-11 18:12:08 +0530

    Create digital_clock.v

---
+++ Digital_Clock/digital_clock.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module digital_clock(
+    input clk,
+    input rst,
+    output reg [5:0] sec,
+    output reg [5:0] min,
+    output reg [4:0] hr
+    );
+    
+    always@(posedge clk, negedge rst) begin
+        if(rst) begin
+            sec <= 0;
+            min <= 0;
+            hr <= 0;
+        end
+        
+        else begin
+            if(sec==59) begin
+                sec <= 0;
+                if(min == 59) begin
+                    min <= 0;
+                    if(hr==23)
+                        hr <= 0;
+                    else hr <= hr + 1;
+                end    
+                else min <= min + 1;
+            end
+            else sec <= sec + 1;
+        end
+    end        
+endmodule
